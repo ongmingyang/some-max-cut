fca4cd8c8aa5828befb30e32a5c7e03839cd9419
Ming Yang <mingy@mit.edu>
2016-04-23 21:30:31 -0400

    When sorting input edges, compare ints instead of str
    *This fixes an error when graphs have a large number of edges

--- python/maxCut.py
+++ python/maxCut.py
@@ -8,10 +8,8 @@ from factorTable import FactorTable
 # @param edges    An iterator containing edges
 #
 def max_cut(edges):
-  I, J = zip(*(sorted(row, reverse=True) for row in edges))
-  I = [int(i) for i in I]
-  J = [int(j) for j in J]
-  c = ct.graph_to_clique_tree(I,J)
+  J, I = zip(*(sorted((int(i),int(j))) for i,j in edges))
+  c = ct.graph_to_clique_tree(list(I),list(J))
   clique_id = 0
   m = bp.max_marginal(c, clique_id)
   bp.traceback(c, clique_id, m)
